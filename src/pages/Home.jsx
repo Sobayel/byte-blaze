4553668ec3be101c68c2828759b396a0fa6f5b27
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-03-23 19:30:37 +0600

    added cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
+import wave from "../assets/wave.svg"
 
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+        <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-116px)]">
             <Hero></Hero>
+            <img className="absolute w-full bottom-0" src={wave} alt="" />
         </div>
     );
 };
